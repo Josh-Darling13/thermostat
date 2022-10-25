971520fad026ea944cfdb15b1473af03749dcb73
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-25 19:35:17 -0400

    added JSX for basic layout

--- src/Main.js
+++ src/Main.js
@@ -5,15 +5,24 @@ export default function Main(){
 
     return(
         <div>
-
-            <h1>
-                This verifies there is something coming in from the Main.js file
-            </h1>
-
-
+            <div className="thermo">
+                <div className="display">
+                    <div className="facts">
+                        facts
+                    </div>
+                    <div className="temp">
+                        <span className="celci">C</span>
+                        <span className="faren">F</span>
+                    </div>
+                    <div className="dad-alert">
+                        Dad senses around the world are tingling now that you've touched the thermostat!
+                    </div>
+                </div>
+                <div className="controls">
+                    <div className="hot">+</div>
+                    <div className="cold">-</div>
+                </div>
+            </div>
         </div>
-
-
     )
-
 }
