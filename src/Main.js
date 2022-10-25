8557ee43d717b777f8dd70d3cba21dfac276995c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-25 19:12:04 -0400

    added Main.js component

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Main(){
+
+
+    return(
+        <div>
+
+            <h1>
+                This verifies there is something coming in from the Main.js file
+            </h1>
+
+
+        </div>
+
+
+    )
+
+}
