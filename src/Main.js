d517602842559afd1643da0890ced8c6f91f6d44
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-25 21:38:30 -0400

    added html and start of css

--- src/Main.js
+++ src/Main.js
@@ -4,23 +4,43 @@ export default function Main(){
 
 
     return(
-        <div>
+        <div className="thewall">
             <div className="thermo">
                 <div className="display">
                     <div className="facts">
-                        facts
+                       <p> Water boils</p>
                     </div>
                     <div className="temp">
-                        <span className="celci">C</span>
-                        <span className="faren">F</span>
+                        {/* add divs here */}
+                    <h1>
+                        <span className="celci">
+                            C
+                        </span>°
+
+                        <span className="faren">
+                            F
+                        </span>°
+                    </h1>
                     </div>
+
+
                     <div className="dad-alert">
-                        Dad senses around the world are tingling now that you've touched the thermostat!
+                        <p>
+                            Dad senses around the world are tingling now that you've touched the thermostat!
+                        </p>
                     </div>
                 </div>
                 <div className="controls">
-                    <div className="hot">+</div>
-                    <div className="cold">-</div>
+                    <div className="hot">
+                        <p>
+                            +
+                        </p>
+                        </div>
+                    <div className="cold">
+                        <p>
+                            -
+                        </p>
+                        </div>
                 </div>
             </div>
         </div>
